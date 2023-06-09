4fbac8cb9e8563347b12e553517ceecbc62b8bfb
RyanTan8 <106262582+RyanTan8@users.noreply.github.com>
2023-06-09 19:45:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,35 +6,50 @@ var url = require('url');
 let chorus = require('./init');
 const PORT = process.env.PORT || 3030;
 
-var file = new(static.Server)(__dirname);
+var file = new (static.Server)(__dirname);
 
 /* Create an HTTP server to handle responses */
 let handleRequest = (request, response) => {
   var q = url.parse(request.url, true);
   var filename = "." + q.pathname;
   var pathname = url.parse(request.url).pathname;
-  if (pathname != '/' && pathname.length == 23) {
-    response.writeHead(200, { 'Content-Type': 'text/enriched', 'Content-Length': ''});
-    var songID = url.parse(request.url).pathname.substr(1);
-    response.write('Song ID: ' + songID + '\n');
-    finalChorus = chorus.getChorus(songID).then(async value => {
-      response.write(value);
-      response.end();
-    })
+  if (pathname.endsWith('.css')) {
+    response.writeHead(200, {
+      'Content-Type': 'text/css',
+    });
+    fs.createReadStream(`.${pathname}`).pipe(response);
+    return;
   }
-  else {
-    //response.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('./index.html', null, function (error, data) {
-      if (error) {
-          response.writeHead(404);
-          response.write('Whoops! File not found!');
-      } else {
-          response.write(data);
-          // response.write(pathname);
-      }
+  if (pathname != '/' && pathname.length == 23) {
+    var songID = url.parse(request.url).pathname.substr(1);
+
+    chorus.getChorus(songID).then(async value => {
+      var htmlContent = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/style.css">
+        <title>Spotify Chorus Finder</title>
+      </head>
+      <body>
+        <h4>Song ID: ${songID}</h4>
+        ${value}
+      </body>
+    </html>
+  `;
+      var contentLength = Buffer.byteLength(htmlContent, 'utf-8');
+      htmlContent += ('<p>' + value + '</p>');
+      htmlContent += ('<h4>Song ID: ' + songID + '</h4>');
+
+      response.setHeader('Cache-Control', 'no-store');
+      response.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': contentLength });
+      response.write(htmlContent);
       response.end();
     });
   }
+  
 };
 
 server = http.createServer(handleRequest).listen(PORT);
